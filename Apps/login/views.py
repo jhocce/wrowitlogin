739df3c137be5a0bd1adf4fa3a1bee40cb204842
jhocce <jhocce3022@hotmail.com>
2021-11-05 10:00:06 -0430

    v017

--- Apps/login/views.py
+++ Apps/login/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from django.views.generic import ListView, View, RedirectView
 from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.adrule import AdRule
+from facebook_business.api import FacebookAdsApi
+
 
 
 
@@ -53,74 +57,88 @@ class returnface(View):
 		return super(returnface, self).dispatch(request,*args, **kwargs )
 	def get(self, request,  *args, **kwargs):
 		
-		code = self.request.GET.get('code')
+		# code = self.request.GET.get('code')
 
 		# Copyright 2014 Facebook, Inc.
 
 		
-		from facebook_business.adobjects.adaccount import AdAccount
-		from facebook_business.adobjects.adrule import AdRule
-		from facebook_business.api import FacebookAdsApi
-
-		access_token = code
-		app_secret = 'f5f111abebde1a9a94ae282de45ce0ef'
-		ad_account_id = 'act_174036684896239'
-		schedule_interval = 'DAILY'
-		entity_type = 'CAMPAIGN'
-		notification_user_id = '4453085001435900'
-		filter_field = 'reach'
-		filter_value = '1'
-		filter_operator = 'GREATER_THAN'
-		app_id = '457901605665005'
-		FacebookAdsApi.init(access_token=access_token)
-
-		fields = [
-		]
-		params = {
-		    'name': 'Sample SDK Rule',
-		    'schedule_spec': { 'schedule_type': schedule_interval },
-		    'evaluation_spec': { 'evaluation_type': 'SCHEDULE', 'filters': [ { 'field': filter_field, 'value': filter_value, 'operator': filter_operator }, { 'field': 'entity_type', 'value': entity_type, 'operator': 'EQUAL' }, { 'field': 'time_preset', 'value': 'LIFETIME', 'operator': 'EQUAL' } ] },
-		    'execution_spec': { 'execution_type': 'NOTIFICATION', 'execution_options': [ { 'field': 'user_ids', 'value': [notification_user_id], 'operator': 'EQUAL' } ] },
-		}
-		print( )
-
-
-		return HttpResponse("prueba->{0}".format(AdAccount(ad_account_id).create_ad_rules_library(
-		    fields=fields,
-		    params=params,
-		)))
 		
-
-
-
-
-		# appkey = '32934847'
-		# secret = 'c394309ac3257ee7790787e19c6ca07d'
-		# url_auth = 'https://oauth.aliexpress.com/token';
+		# access_token = code
+		client_id = '457901605665005'
+		client_secret = 'f5f111abebde1a9a94ae282de45ce0ef'
+   		code = self.request.GET.get('code')
+		redirect_uri = 'https://wrowit.herokuapp.com/login/returnface/'
+		url_auth = 'https://graph.facebook.com/v12.0/oauth/access_token'
 		# auth_data = {
-		#  'grant_type':'authorization_code',
-		#  'client_id':appkey,
-		#  'client_secret':secret,
+		#  'client_id':client_id,
+		#  'client_secret':client_secret,
 		#  'code':code,
-		#  'sp':'ae',
-		#  'redirect_uri':'redirect_uri'
+		#  'redirect_uri':redirect_uri
 		#  }
 
-		# # se pide el access_token.
-		# resp = requests.post(url_auth, data=auth_data )
+		# se pide el access_token.
+		resp = requests.get('{0}?client_id={1}&client_secret={2}&code={3}&redirect_uri={4}'.format(url_auth, client_id, client_secret, code, redirect_uri ) )
 
-		# try:
-		# 	print("Response---->>> ",resp.json())
-		# 	obj = Permisos(**resp.json())
-		# 	obj.save()
-		# 	access_token = resp.json()['access_token']
-		# except Exception as e:
-		# 	print(e)
-		# 	return HttpResponse("Ya no puedes iniciar sesion desde este enlace... ")
+		try:
+			print("Response---->>> ",resp.json())
+			return HttpResponse(resp.json())
+			# obj = Permisos(**resp.json())
+			# obj.save()
+			# access_token = resp.json()['access_token']
+		except Exception as e:
+			print(e)
+			return HttpResponse(e)
+
+		# First we try to get all pages from a user
+		# fields = [
+		#     'access_token',
+		# ]
+		# params = {
+		# }
+		# pages = User(<USER_ID>).get_accounts(
+		#     fields=fields,
+		#     params=params,
+		# )
+		# page_id = pages[0].get_id()
+		# print(pages[0].get_id())
+
+		# # Then we get first page's insight using page access token
+		# FacebookAdsApi.init(<ACCESS_TOKEN>=pages[0]["access_token"])
+		# fields = [
+		# ]
+		# params = {
+		#     'metric': 'page_fan_adds',
+		# }
+		# insights = Page(page_id).get_insights(
+		#     fields=fields,
+		#     params=params,
+		# )
+		# FacebookAdsApi.init(<ACCESS_TOKEN>=access_token)
 
 
+		# app_secret = 'f5f111abebde1a9a94ae282de45ce0ef'
+		# ad_account_id = 'act_174036684896239'
+		# schedule_interval = 'DAILY'
+		# entity_type = 'CAMPAIGN'
+		# notification_user_id = '4453085001435900'
+		# filter_field = 'reach'
+		# filter_value = '1'
+		# filter_operator = 'GREATER_THAN'
+		# app_id = '457901605665005'
+		# FacebookAdsApi.init(access_token=access_token)
+
+		# fields = [
+		# ]
+		# params = {
+		#     'name': 'Sample SDK Rule',
+		#     'schedule_spec': { 'schedule_type': schedule_interval },
+		#     'evaluation_spec': { 'evaluation_type': 'SCHEDULE', 'filters': [ { 'field': filter_field, 'value': filter_value, 'operator': filter_operator }, { 'field': 'entity_type', 'value': entity_type, 'operator': 'EQUAL' }, { 'field': 'time_preset', 'value': 'LIFETIME', 'operator': 'EQUAL' } ] },
+		#     'execution_spec': { 'execution_type': 'NOTIFICATION', 'execution_options': [ { 'field': 'user_ids', 'value': [notification_user_id], 'operator': 'EQUAL' } ] },
+		# }
+		# print( )
 
 
-
-# code=AQAKsoeFqLl8uncyc3nHP7BO3yPOfoH-y9b6iLOj_4HZWObC4C1yZB-kShxGAXSorKJPF1ncvUseMcoDVRw2mkhUELFpS7rFcq4vh3n-6SbTkjo9d-NrkYyCZwcUJ8rpC8w5SBVYnLo1L5Kp_4_ZKpqtpMZEOmu1R36Kw7LoqIbcHVYUDmVDLc38dKuEzS9-Sq8Bhw7lYkNaDcdFayQPDxcP5fBLVLXSWn0yOeNd-8nYuMp9R3SfI1yW0AxxMPG6mPNJ-iYz5aV5rieXBWX0SzNfyhuM3d29i8Mq5GTmQcUa7bOGLtiSecGP3hM_i98aLANi8NLWCIw3OPKbd2at-6mWQcQzgHvamUnDxOILVMoWPWc6on8Z4ux9g6sNH5tpPn0
-# &state=1212#_=_
+		# return HttpResponse("prueba->{0}".format(AdAccount(ad_account_id).create_ad_rules_library(
+		#     fields=fields,
+		#     params=params,
+		# )))
