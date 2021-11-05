ef4cda4235554509024184e302e52e2ad1652277
jhocce <jhocce3022@hotmail.com>
2021-11-05 07:24:19 -0430

    v005

--- wrowit/settings.py
+++ wrowit/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).ancestor(2)
 
 
 # Quick-start development settings - unsuitable for production
