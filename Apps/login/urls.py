45f11c254b09e8df019bf450e071554d57492484
jhocce <jhocce3022@hotmail.com>
2021-11-06 11:38:27 -0430

    V049

--- Apps/login/urls.py
+++ Apps/login/urls.py
@@ -4,7 +4,7 @@ from .views import loginface, returnface, googlev
 urlpatterns = [
 	url(r'^face/$', loginface.as_view(), name="loginface"),
 	url(r'^returnface/$', returnface.as_view(), name="returnface"),
-	url(r'^$', googlev.as_view(), name="googlev"),
+	url(r'^googlee41be98abcadb5cd.html$', googlev.as_view(), name="googlev"),
 
 	# url(r'^ver/(?P<pk_publica>[^/]+)/$', DetallarProducto.as_view(), name="ver"),
 	# url(r'^eliminar/$', CrearUserView.as_view(), name="eliminar"),
