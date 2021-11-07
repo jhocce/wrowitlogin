f3385489cf4809f7636344bb326f795dbf0b6fc5
jhocce <jhocce3022@hotmail.com>
2021-11-07 11:54:01 -0430

    V050

--- Apps/goo/urls.py
+++ Apps/goo/urls.py
@@ -1,11 +1,11 @@
 
 from django.conf.urls import url
-from .views import goo, googlev
+from .views import goo, googlev, redirect
 
 urlpatterns = [
 
 	url(r'^goo/$', goo.as_view(), name="goo"),
-	url(r'^redirect/$', goo.as_view(), name="goore"),
+	url(r'^redirect/$', redirect.as_view(), name="goore"),
 	url(r'^googlee41be98abcadb5cd.html$', googlev.as_view(), name="googlev"),
 	# url(r'^face/$', loginface.as_view(), name="loginface"),
 	# url(r'^returnface/$', returnface.as_view(), name="returnface"),
