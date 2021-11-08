410d1f5a6423deba28601b839ff34d0d2dd24a4c
jhocce <jhocce3022@hotmail.com>
2021-11-08 07:15:46 -0430

    V064

--- Apps/goo/views.py
+++ Apps/goo/views.py
@@ -67,15 +67,18 @@ class redirectgo(View):
 		
 		# credentials = go.credentials
 
-
-		data = requests.post('https://www.googleapis.com/oauth2/v4/token',
-		{
-			'code' : code,
-			'client_id' : '558376713536-hehho8pmk7lcbn7vumtmstikpjat85s6.apps.googleusercontent.com',
-			'client_secret' : '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',
-			'redirect_uri' : 'https://wrowit.herokuapp.com/',
-			'grant_type' : 'authorization_code'
-		})
+		try:
+			data = requests.post('https://www.googleapis.com/oauth2/v4/token',
+				{
+					'code' : code,
+					'client_id' : '558376713536-hehho8pmk7lcbn7vumtmstikpjat85s6.apps.googleusercontent.com',
+					'client_secret' : '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',
+					'redirect_uri' : 'https://wrowit.herokuapp.com/',
+					'grant_type' : 'authorization_code'
+				})
+		except Exception as e:
+			data = e
+		
 
 		return HttpResponse("---->> {0} ".format(data) )
 
