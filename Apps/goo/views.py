b9f85cd27d67de347f96f810371257039d71e8fb
jhocce <jhocce3022@hotmail.com>
2021-11-08 13:57:24 -0430

    V104

--- Apps/goo/views.py
+++ Apps/goo/views.py
@@ -40,7 +40,7 @@ class goo(View):
 
 		flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
 		   client_secrets_file='client_secret.json',
-		    scopes = ('https://www.googleapis.com/auth/userinfo.email', 'openid')
+		    scopes = ('https://www.googleapis.com/auth/adwords', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', 'openid')
 		     )
 		flow.redirect_uri = 'https://wrowit.herokuapp.com/google/redirect/'
 		authorization_url, state = flow.authorization_url(
@@ -89,7 +89,7 @@ class redirectgo(View):
 		print("pppppppppppppppppppp",scope)
 		flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
 		   client_secrets_file='client_secret.json',
-		   scopes = ('https://www.googleapis.com/auth/userinfo.email', 'openid')
+		   scopes = ('https://www.googleapis.com/auth/adwords', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', 'openid')
 		     )
 		flow.redirect_uri = 'https://wrowit.herokuapp.com/google/redirect/'
 
